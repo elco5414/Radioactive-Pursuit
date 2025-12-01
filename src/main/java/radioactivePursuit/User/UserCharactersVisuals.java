5d0e32bf1877553db72965dd2a7d585129915f24
elco5414 <elco5414@colorado.edu>
2025-12-01 16:50:32 -0700

    scientist visual

--- src/main/java/radioactivePursuit/User/UserCharactersVisuals.java
+++ src/main/java/radioactivePursuit/User/UserCharactersVisuals.java
@@ -5,10 +5,18 @@ import radioactivePursuit.player.Player;
 public class UserCharactersVisuals {
     private void displayScientistArt() {
         //fill in with the scientist ASCII character here
+        System.out.println(
+                "    (ᵔ‿ᵔ)✨\n" +
+                "    /|️🔬|\\\n" +
+                " 💉/ |  | \\\n" +
+                "    /|  |\\\n" +
+                "   /_|__|_\\\n" +
+                "     /  \\\n"
+        );
     }
 
     public void displayScientist(Player scientist) {
-        System.out.println(scientist.getName() + " has " + scientist.getAntidoteCount() + "antidotes, and " + scientist.getHealth() + " health.\n")
+        System.out.println(scientist.getName() + ": " + scientist.getAntidoteCount() + "🧪, " + scientist.getHealth() + "❤️.\n")
         displayScientistArt();
     }
 }
