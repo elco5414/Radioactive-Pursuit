1dd4338537700f5b88637e338a89a17f0306493a
Jacey Fischer <jafi1775@colorado.edu>
2025-12-03 10:55:37 -0700

    starting planet test

--- src/test/java/radioactivePursuit/planet/PlanetTest.java
+++ src/test/java/radioactivePursuit/planet/PlanetTest.java
@@ -1,33 +1,30 @@
 package radioactivePursuit.planet;
 
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import radioactivePursuit.creatures.CreatureFactory;
 import radioactivePursuit.interactives.ArtifactFactory;
+import radioactivePursuit.player.Player;
 
-import java.util.List;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class PlanetTest {
     private final ArtifactFactory artifactFactory = new ArtifactFactory();
     private final CreatureFactory creatureFactory= new CreatureFactory();
     private final BiomeFactory biomeFactory = new BiomeFactory(artifactFactory, creatureFactory);
 
-//    @Test
-//    void testToString() {
-//        List<Character> characters = List.of(
-//                creatureFactory.createKnight("Bilbo"),
-//                creatureFactory.createCreature("Ogre"));
-//        Planet planet = Planet.getNewBuilder(biomeFactory)
-//                .create2x2Grid()
-//                .addCharacters(characters)
-//                .build();
-//        String mazeString = maze.toString();
-//        logger.info(mazeString);
-//
-//        assertTrue(mazeString.contains("Bilbo"));
-//        assertTrue(mazeString.contains("Ogre"));
-//    }
+    @Test
+    void TestNumRooms() {
+        Player player = new Player("TestPlayer");
+
+        Planet planet = Planet.getNewBuilder(biomeFactory)
+                .createBiomes(10)
+                .connectCirclePlanet()
+                .add(player)
+                .build();
+
+        assertEquals(10, planet.size());
+
+    }
 }
