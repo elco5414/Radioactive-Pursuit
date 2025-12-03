70b639115f92ea676f8a719cf1bdff0916c496c2
Jacey Fischer <jafi1775@colorado.edu>
2025-12-03 15:52:39 -0700

    background story

--- src/main/java/radioactivePursuit/Main.java
+++ src/main/java/radioactivePursuit/Main.java
@@ -23,6 +23,7 @@ public class Main {
         Player currentPlayer = new Player(currentUser.getName());
         Planet currentPlanet = worldSetUp(currentPlayer);
 
+        openingDisplay(currentPlayer);
         //while loop to actually play the game
         while(gameIsNotOver(currentPlayer, currentPlanet)){
             playGame();
@@ -69,9 +70,23 @@ public class Main {
         //call display function
     }
 
-    private static void openingDisplay(){
+    private static void openingDisplay(Player player){
         //fill in the background story here...
-        System.out.println("background .... story ...\n");
+        System.out.println("Many decades ago, Earth fell silent. A chain of nuclear failures poisoned the land, twisted the wildlife, and forced humanity to flee. A handful of scientists escaped into orbit, watching their home decay from above while they struggled to survive.\n" +
+                "\n" +
+                "Generations passed, and now only fragmented transmissions reach the stations: life still clings to the surface, but it has changed. Animals once familiar have become dangerous, aggressive, and radioactive. If nothing is done, Earth will be lost forever.\n" +
+                "\n" +
+                "You, " + player.getName()+ ", are the first scientist brave — or desperate — enough to return.\n" +
+                "\n" +
+                "Armed with research, limited equipment, and a supply of experimental antidotes, you descend toward the ruined planet. Your mission is simple but merciless:\n" +
+                "\n" +
+                "• Cure what you can.\n" +
+                "• Eliminate what you cannot.\n" +
+                "\n" +
+                "The antidotes you carry are few, and although rare supplies may still exist scattered across Earth, they will not be enough to save everything. Hard choices await. Some creatures can be restored… others must be destroyed before their mutations spread beyond control.\n" +
+                "\n" +
+                "Step outside your ship, " + player.getName() + ".\n" +
+                "Earth is broken, but hope has landed with you.");
     }
 
     private static void finalDisplay() {
